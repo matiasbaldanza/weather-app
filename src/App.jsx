c78f2b0aa844a53946c9c0c8f656651a4e12519b
Matias Baldanza <matiasbaldanza@gmail.com>
2023-03-13 13:19:02 -0300

    FEAT: basic data fetch (local)

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,32 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './App.css'
+import { api } from './api'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [currentWeather, setCurrentWeather] = useState(null)
+  const { location, current } = currentWeather ?? {} 
+
+  useEffect(() => {
+    api.current_local()
+      .then((response) => response.json())
+      .then((data) => setCurrentWeather(data))  
+  }, [])
 
   return (
-    <p>Weather App</p>
+      <div className="container">
+        { !currentWeather 
+            ? <p>Loading...</p> 
+            : <>
+                <header>
+                  <h1>{location.name}, {location.region}, {location.country}</h1>
+                </header>
+                <section>
+                  <p>Temp: <span>{current.temp_c}</span></p>
+                  <p>Feels Like: <span>{current.feelslike_c}</span></p>
+                </section>
+              </>
+        }
+      </div>
   )
 }
 
